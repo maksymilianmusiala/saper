1f4976f575e833da1c93db46cf3d326987d616a9
Maksymilian <maksymilian.musiala@gmail.com>
2026-08-27 22:15:10 +0200

    play in terminal

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
-#include <iostream>
-#include "MinesweeperBoard.h"
-
+#include "MineSweeperBoard.h"
+#include "MSBoardTextView.h"
+#include "MSTextController.h"
 
 int main() {
+    srand(time(nullptr));
+    // int row, col;
+    // GameMode mode;
+    // std::cout << "Wprowadź wysokość i szerokość planszy, na ktorej chcesz grac:" << std::endl;
+    // std::cout << "Wysokosc (z zakresu 0-100): ";
+    // std::cin >> row;
+    // std::cout << "Szerokosc (z zakresu 0-100): ";
+    // std::cin >> col;
+    // system("clear");
 
-    MinesweeperBoard board(10, 10, DEBUG);
-    board.debug_display();
+    MinesweeperBoard board(10, 10, GameMode::DEBUG);
+    MSBoardTextView view(board);
+    MSTextController ctrl(board, view);
 
+    ctrl.play();
+
+    return 0;
 }
